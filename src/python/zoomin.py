487c6ab4326ec8f95723a6033d7d4ad68904fdef
Terence Tao <tao@math.ucla.edu>
2025-04-30 16:18:41 -0700

    add new floor+residuals data

--- src/python/zoomin.py
+++ src/python/zoomin.py
@@ -25,16 +25,18 @@ base2 = []
 t_lower2 = []
 t_upper2 = []
 t_lower_ip = []
+t_lower_res = []
 
 with open('../../data/lp_bounds_40000-45000.txt', 'r') as f:
     next(f)
     for line in f:
         nums = re.findall(r"-?\d+(?:\.\d+)?", line)
-        a, b, c, d, e = int(nums[0]), int(nums[1]), int(nums[2]), int(nums[3]), float(nums[4])
+        a, b, c, d, e, f = int(nums[0]), int(nums[1]), int(nums[2]), int(nums[3]), int(nums[4]), float(nums[5])
         base2.append(a)
         t_lower2.append(c)
-        t_lower_ip.append(d)
-        t_upper2.append(e)
+        t_lower_res.append(d)
+        t_lower_ip.append(e)
+        t_upper2.append(f)
         if d < a and a >= 43632:
             print(f"Ad hoc IP lower bound {d} for {a} is below the floor {c} at N={a}!")
         
@@ -45,18 +47,20 @@ def plot():
     ub = [t_upper[i]-base[i] for i in range(len(base))]
     lb2 = [t_lower2[i]-base2[i] for i in range(len(base2))]
     ub2 = [t_upper2[i]-base2[i] for i in range(len(base2))]
-    lb_ip = [t_lower_ip[i]-base2[i] for i in range(len(base2))]
+    lb_ip = [t_lower_ip[i]-base2[i]-0.01 for i in range(len(base2))]
+    lb_res = [t_lower_res[i]-base2[i]+0.01 for i in range(len(base2))]
     plt.figure(figsize=[8, 6])
 #    plt.plot(base, ub, label='Smoothfac (upper)', marker='.', markersize=3, color='green') 
     plt.plot(base2, ub2, label='Linear programming (upper)', marker='.', markersize=3, color='red') 
-    plt.plot(base2, lb2, label='LP floor (lower)', marker='.',linestyle='None', markersize=3, color='blue' )
-    plt.plot(base, lb, label='Smooth factorization (lower)', marker='.',linestyle='None', markersize=3, color='purple' )
+#    plt.plot(base2, lb2, label='LP floor (lower)', marker='.',linestyle='None', markersize=3, color='crimson' )
+    plt.plot(base, lb, label='Smooth factorization (lower)', marker='.',linestyle='None', markersize=3, color='orange' )
+    plt.plot(base2, lb_res, label='LP floor+residuals (lower)', marker='.',linestyle='None', markersize=3, color='blue' )
     plt.plot(base2, lb_ip, label='Ad hoc IP (lower)', marker='.',linestyle='None', markersize=3, color='green' )
 #    plt.plot(base, [0 for _ in base], label='$0$',  color='purple')
     plt.title('Surplus factors $M(N,N/3)-N$ in $N/3$-admissible factorization of N!')
     plt.xlabel('$N$')
     plt.ylabel('$M(N,N/3)-N$')
-    plt.ylim(-12,3)
+    plt.ylim(-3,3)
     plt.legend()
     plt.grid(True)
     plt.show()
